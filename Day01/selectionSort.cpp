e40ccdccd938910451f6ca0d00c587cb849c94c0
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-05-01 20:18:37 +0530

    Create selectionSort.cpp

---
+++ Day01/selectionSort.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        for(int i = 0; i < n-1; i++){
+            int min_id = i;
+            for(int j = i; j < n; j++){
+                if(arr[j] <= arr[min_id])
+                    min_id = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[min_id];
+            arr[min_id] = temp;
+        }
+    }
+};
