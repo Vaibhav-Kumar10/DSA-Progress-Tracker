7296cda26e4a0607e11c7efca25c98b197981c45
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-05-01 20:15:35 +0530

    Create bubbleSort.cpp

---
+++ Day01/bubbleSort.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(vector<int>& arr) {
+        int n = arr.size();
+        for(int i = n-1; i >= 0; i--){
+            int didSwap = 0;
+            for(int j = 0; j <= i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    didSwap = 1;
+                }
+            }
+            if(didSwap == 0)
+                break;
+        }
+    }
+};
