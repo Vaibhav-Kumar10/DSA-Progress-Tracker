8aaa95efc4f764be441313edc465673981d80657
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-05-01 23:14:42 +0530

    Update

--- Day01/insertionSort.cpp
+++ Day01/insertionSort.cpp
@@ -1,13 +1,17 @@
-class Solution {
-  public:
-    //Function to sort the array using insertion sort algorithm.
-    void insertionSort(vector<int>& arr) {
+class Solution
+{
+public:
+    // Function to sort the array using insertion sort algorithm.
+    void insertionSort(vector<int> &arr)
+    {
         int n = arr.size();
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++)
+        {
             int j = i;
-            while(j > 0 && arr[j-1] > arr[j]){
-                int temp = arr[j-1];
-                arr[j-1] = arr[j];
+            while (j > 0 && arr[j - 1] > arr[j])
+            {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
                 arr[j] = temp;
                 j--;
             }
--- Day01/selectionSort.cpp
+++ Day01/selectionSort.cpp
@@ -1,13 +1,17 @@
-class Solution {
-  public:
+class Solution
+{
+public:
     // Function to perform selection sort on the given array.
-    void selectionSort(vector<int> &arr) {
+    void selectionSort(vector<int> &arr)
+    {
         // code here
         int n = arr.size();
-        for(int i = 0; i < n-1; i++){
+        for (int i = 0; i < n - 1; i++)
+        {
             int min_id = i;
-            for(int j = i; j < n; j++){
-                if(arr[j] <= arr[min_id])
+            for (int j = i; j < n; j++)
+            {
+                if (arr[j] <= arr[min_id])
                     min_id = j;
             }
             int temp = arr[i];
