4009a2520569686b1d752d21fbed4da69b11af23
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-05-04 00:48:44 +0530

    Update checkSorted.cpp

--- Day03/checkSorted.cpp
+++ Day03/checkSorted.cpp
@@ -3,14 +3,15 @@ class Solution
 public:
     bool check(vector<int> &nums)
     {
-        for (int i = 0; i < nums.size() - 2; i++)
-        {
+        int n = nums.size(), count = 0;
+        // Checks if array is sorted
+        if (nums[0] < nums[n - 1])
+            count++;
+        // Checks and counts if elements are not in correct order
+        for (int i = 0; i < n - 1; i++)
             if (nums[i] > nums[i + 1])
-            {
-                if (nums[i] < nums[i + 2])
-                    return false;
-            }
-        }
-        return true;
+                count++;
+        // If more than 1 elements are in wrong order, it returns false
+        return count <= 1;
     }
 };
