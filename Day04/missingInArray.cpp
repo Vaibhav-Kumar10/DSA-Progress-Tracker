4b5f9e265aec82206e23b8f49f95eef2e5108274
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-05-22 23:14:29 +0530

    Update missingInArray.cpp

--- Day04/missingInArray.cpp
+++ Day04/missingInArray.cpp
@@ -3,15 +3,22 @@ class Solution
 public:
     int missingNumber(vector<int> &nums)
     {
-        int total = 0, req = 0;
-        for (int i = 1; i <= nums.size(); i++)
+        // int n = nums.size();
+        // int sumN = n * (n + 1) / 2;
+        // int arraySum = 0;
+        // for (int el : nums)
+        //     arraySum += el;
+        // int missing = sumN - arraySum;
+        // return missing;
+
+        int N = nums.size();
+        int xor1 = N, xor2 = 0;
+        for (int i = 0; i < N; i++)
         {
-            total += i;
+            xor1 = xor1 ^ (i);     // XOR up to [1...N-1]
+            xor2 = xor2 ^ nums[i]; // XOR of array elements
         }
-        for (int i = 0; i < nums.size(); i++)
-        {
-            req += nums[i];
-        }
-        return total - req;
+        // xor1 = xor1 ^ N;      // XOR up to [1...N]
+        return (xor1 ^ xor2); // the missing number
     }
 };
